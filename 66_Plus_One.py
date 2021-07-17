888445057b9def94318a6ee48e5b7f9d884cafb6
IAjimi <ia767@nyu.edu>
2021-07-17 07:58:13 -0400

    adding plus one

---
+++ 66_Plus_One.py
@@ -0,0 +1,10 @@
+class Solution:
+    def plusOne(self, digits: List[int]) -> List[int]:
+        '''
+        Top 20% by speed.
+        '''
+        num = int(''.join([str(s) for s in digits]))
+        num += 1
+        num = str(num)
+        num = [int(n) for n in num]
+        return num
