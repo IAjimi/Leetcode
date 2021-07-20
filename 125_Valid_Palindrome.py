b489f9934d69d4d1e8026b4e0489fbe0e30439b1
IAjimi <ia767@nyu.edu>
2021-07-20 07:29:03 -0400

    adding sum root to leaf numbers

--- 125_Valid_Palindrome.py
+++ 125_Valid_Palindrome.py
@@ -1,10 +1,10 @@
 class Solution:
     def isPalindrome(self, s: str) -> bool:
-    	'''
-    	Top 20% by speed.
-    	'''
+        '''
+        Top 20% by speed.
+        '''
         palindrome = [l.lower() for l in s if l.isalpha() or l.isdigit()]
         if palindrome == palindrome[::-1]:
-        	return True
+            return True
         else:
-        	return False
+            return False
