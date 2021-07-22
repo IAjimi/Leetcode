6f5e4edbfa0f37245ae7443b957018c4b7f52a41
IAjimi <ia767@nyu.edu>
2021-07-22 07:26:56 -0400

    adding climbing stairs

---
+++ 70_Climbing_Stairs.py
@@ -0,0 +1,19 @@
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        """
+        Runtime: 24 ms, faster than 95.04% of Python3 online submissions.
+        Memory Usage: 14.3 MB, less than 11.50% of Python3 online submissions.
+        """
+        if n == 1:
+            return 1
+        elif n == 2:
+            return 2
+        else:
+            twice_before = 1
+            once_before = 2
+
+            for ix in range(2, n):
+                steps = once_before + twice_before
+                once_before, twice_before = steps, once_before
+
+            return steps
