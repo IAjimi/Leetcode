79943adebe1a2863672d6730ed2111138ec2cb41
I A <ia767@nyu.edu>
2022-02-15 07:40:58 -0500

    [104] refactoring solution

--- 104_Maximum_Depth_of_Binary_Tree.py
+++ 104_Maximum_Depth_of_Binary_Tree.py
@@ -1,25 +1,23 @@
-# Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
+from typing import Optional
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
 
 class Solution:
-    """
-    Runtime: 28 ms, faster than 99.59% of Python3 online submissions.
-    Memory Usage: 16.1 MB, less than 61.37% of Python3 online submissions.
-    """
-    def maxDepth(self, root: TreeNode) -> int:
-        if root:
-            return self.explore(root, 1)
-        else:
-            return 0
-    
-    def explore(self, root, n):        
-        if not root or (not root.left and not root.right):
-            return n
+    def maxDepth(self, root: Optional[TreeNode], depth: int = 0) -> int:
+        """
+        Runtime: 56 ms, faster than 48.98% of Python3 online submissions for Maximum Depth of Binary Tree.
+        Memory Usage: 16.4 MB, less than 8.18% of Python3 online submissions for Maximum Depth of Binary Tree.
+        """
+        if not root:
+            return depth
 
-        left_n = self.explore(root.left, n + 1)
-        right_n = self.explore(root.right, n + 1)
-        return max(left_n, right_n)
+        depth += 1
+        left_depth = self.maxDepth(root.left, depth) if root.left else depth
+        right_depth = self.maxDepth(root.right, depth) if root.right else depth
+
+        return max(left_depth, right_depth)
+
