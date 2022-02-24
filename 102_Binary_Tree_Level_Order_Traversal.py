05a89e9c9c21e48a8e7d13a121bf996a05c7eb72
I A <ia767@nyu.edu>
2022-02-24 07:35:26 -0500

    [102] refactor solution

--- 102_Binary_Tree_Level_Order_Traversal.py
+++ 102_Binary_Tree_Level_Order_Traversal.py
@@ -1,48 +1,38 @@
 # Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
+from typing import Optional, List
+
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
 class Solution:
-	def __init__(self):
-		self.path = []
+    def levelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
+        """
+        Runtime: 36 ms, faster than 83.53% of Python3 online submissions for Binary Tree Level Order Traversal.
+        Memory Usage: 14.2 MB, less than 94.90% of Python3 online submissions for Binary Tree Level Order Traversal.
+        """
+        if not root:
+            return []
 
-	def levelOrder(self, root):
-		"""BFS approach.
+        path = []
+        queue = [(0, root)]
 
-		Runtime: 32 ms, faster than 84.13% of Python3 online submissions.
-		Memory Usage: 14.4 MB, less than 87.06% of Python3 online submissions.
-		"""
-		visited = []
+        while queue:
+            level, node = queue.pop(0)
 
-		if root:
-			queue = [(root, 1)]
+            if not node:
+                continue
+            else:
+                if len(path) <= level:
+                    path.append([node.val])
+                else:
+                    path[level].append(node.val)
 
-			while queue:
-				element, level = queue.pop(0)
-				if len(visited) < level:
-					visited.append([element.val])
-				else:
-					visited[level - 1].append(element.val)
+            queue.append((level + 1, node.left))
+            queue.append((level + 1, node.right))
 
-				if element.left:
-					queue.append((element.left, level + 1))
-				if element.right:
-					queue.append((element.right, level + 1))
-
-		return visited
-
-	def levelOrder(self, root: TreeNode) -> List[List[int]]:
-		"""
-		Input [3,9,20,null,null,15,7]
-		Output
-		[[3,9,20,15,7]]
-		Expected
-		[[3],[9,20],[15,7]]"""
-		self.path.append(root.val)
-		if root.left:
-			self.levelOrder(root.left)
-		if root.right:
-			self.levelOrder(root.right)
-		return [self.path]
+        return path
