72e4adf09a0dfb28d271c320696e3e92c81e8bde
I A <ia767@nyu.edu>
2022-03-01 08:13:30 -0500

    [169] add new solution, lint code

--- 169_Majority_Element.py
+++ 169_Majority_Element.py
@@ -1,41 +1,43 @@
+from collections import defaultdict
+from typing import List
+
+
 class Solution:
-	def majorityElement(self, nums: List[int]) -> int:
-		"""
-		Attempt to run in O(n) with O(1) memory.
+    def majorityElement(self, nums: List[int]) -> int:
+        """
+        O(n) time, O(1) space.
 
-		Runtime: 188 ms, faster than 24.24% of Python3 online submissions.
-		Memory Usage: 15.5 MB, less than 82.30% of Python3 online submissions.
-		"""
-		nums.sort()
-		cur_count, max_count = 0, 0
-		max_val = nums[0]
+        Runtime: 227 ms, faster than 50.36% of Python3 online submissions for Majority Element.
+        Memory Usage: 15.5 MB, less than 81.81% of Python3 online submissions for Majority Element.
+        """
+        nums.sort()
+        counter = 0
+        cur_val = None
 
-		for ix in range(1, len(nums)):
-			if nums[ix] != nums[ix - 1] or ix == len(nums) - 1:
-				max_val = nums[ix - 1] if cur_count > max_count else max_val
-				max_count = max(max_count, cur_count)
-				cur_count = 1
-			else:
-				cur_count += 1
+        for val in nums:
+            if val != cur_val:
+                counter = 0  # reset counter
+                cur_val = val
 
-			if max_count >= 1 + (len(nums) / 2):
-				return max_val
+            counter += 1
 
-		return max_val
+            if counter >= 1 + (len(nums) // 2):
+                return val
 
-	def AltMajorityElement(self, nums: List[int]) -> int:
-		"""
-		Faster but uses more memory.
+    def AltMajorityElement(self, nums: List[int]) -> int:
+        """
+        Faster implementation (still O(n)) but uses more memory (O(m) where m is the number
+        of distinct values in nums).
 
-		Runtime: 180 ms, faster than 31.31% of Python3 online submissions for Majority Element.
-		Memory Usage: 15.6 MB, less than 12.08% of Python3 online submissions for Majority Element.
-		"""
-		nums.sort()
-		counter = defaultdict(int)
+        Runtime: 180 ms, faster than 31.31% of Python3 online submissions for Majority Element.
+        Memory Usage: 15.6 MB, less than 12.08% of Python3 online submissions for Majority Element.
+        """
+        nums.sort()
+        counter = defaultdict(int)
 
-		for val in nums:
-			counter[val] += 1
-			if counter[val] >= 1 + (len(nums) // 2):
-				return val
+        for val in nums:
+            counter[val] += 1
+            if counter[val] >= 1 + (len(nums) // 2):
+                return val
 
-		return -1
+        return -1
