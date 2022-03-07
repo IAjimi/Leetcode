8b29ba641aeb920ec3827455973529b42fa3f490
I A <ia767@nyu.edu>
2022-03-07 07:57:12 -0500

    [118] refactor solution

--- 118_Pascal_Triangle.py
+++ 118_Pascal_Triangle.py
@@ -1,27 +1,19 @@
+from typing import List
+
+
 class Solution:
-	def generate(self, numRows: int) -> List[List[int]]:
-		"""
-		Runtime: 32 ms, faster than 60.19% of Python3 online submissions.
-		Memory Usage: 14 MB, less than 94.62% of Python3 online submissions.
-		"""
-		pascal_triangle = [[1], [1, 1], [1, 2, 1]]
+    def generate(self, numRows: int) -> List[List[int]]:
+        """
+        Runtime: 40 ms, faster than 61.87% of Python3 online submissions for Pascal's Triangle.
+        Memory Usage: 13.9 MB, less than 52.79% of Python3 online submissions for Pascal's Triangle.
+        """
+        rows = [[1]]
 
-		if numRows <= 3:
-			return pascal_triangle[:numRows]
-		else:
-			for row in range(4, numRows + 1):
-				current_row = []
+        for n in range(1, numRows):
+            newRow = [
+                rows[n - 1][i] + rows[n - 1][i + 1] for i in range(len(rows[n - 1]) - 1)
+            ]
+            newRow = [1] + newRow + [1]
+            rows.append(newRow)
 
-				for ix in range(row):
-					if ix == 0:  # 1st element
-						current_row.append(1)
-					elif ix == row - 1:  # last element
-						current_row.append(1)
-					else:
-						new_val = pascal_triangle[-1][ix - 1] + pascal_triangle[-1][ix]
-						current_row.append(new_val)
-
-				pascal_triangle.append(current_row)
-
-			return pascal_triangle
-
+        return rows
