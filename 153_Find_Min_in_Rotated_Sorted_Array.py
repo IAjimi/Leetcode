286be9e2e5c526a9b7931a1d22d36f1493cd13c4
I A <ia767@nyu.edu>
2022-03-11 07:29:25 -0500

    [153] refactor solution

--- 153_Find_Min_in_Rotated_Sorted_Array.py
+++ 153_Find_Min_in_Rotated_Sorted_Array.py
@@ -1,35 +1,39 @@
-class Solution:
-    def findMin(self, nums):
-        """
-        Binary search type thing.
+from typing import List
 
-        Runtime: 44 ms, faster than 39.47% of Python3 online submissions.
-		Memory Usage: 14.7 MB, less than 28.49% of Python3 online submissions.
+
+class Solution:
+    def findMin(self, nums: List[int]) -> int:
+        """
+        Runtime: 39 ms, faster than 92.75% of Python3 online submissions for Find Minimum in Rotated Sorted Array.
+        Memory Usage: 14.1 MB, less than 78.41% of Python3 online submissions for Find Minimum in Rotated Sorted Array.
         """
         if len(nums) == 1:
             return nums[0]
-        elif len(nums) == 2:
-            return nums[0] if nums[0] < nums[1] else nums[1]
-        else:
-            left, right = 0, len(nums) - 1
 
-            while right - left > 1:  # can get stuck for odd numbered arrays
-                if nums[right] >= nums[left]:
-                    right = (right + left) // 2
-                else:
-                    left = (right + left) // 2
+        left = 0
+        right = len(nums) - 1
 
-            while nums[left] > nums[left - 1]:
-                left += -1
+        while 0 <= left <= right <= len(nums) - 1:
+            mid = (left + right) // 2
 
-            return nums[left]
+            if mid > 0 and nums[mid] < nums[mid - 1]:
+                return nums[mid]
+            elif mid == 0 and nums[mid] < nums[mid + 1]:
+                return nums[mid]
+            elif nums[mid] > nums[right]:
+                left = mid + 1
+            else:
+                right = mid - 1
 
-if __name__ == '__main__':
+        return -1
+
+
+if __name__ == "__main__":
     assert 0 == Solution().findMin([0])
-    assert 1 == Solution().findMin([1,2])
-    assert 1 == Solution().findMin([2,1])
-    assert 1 == Solution().findMin([3,1,2])
-    assert 1 == Solution().findMin([2,3,1])
-    assert 1 == Solution().findMin([3,4,5,1,2])
-    assert 0 == Solution().findMin([4,5,6,7,0,1,2])
-    assert 11 == Solution().findMin([11,13,15,17])
+    assert 1 == Solution().findMin([1, 2])
+    assert 1 == Solution().findMin([2, 1])
+    assert 1 == Solution().findMin([3, 1, 2])
+    assert 1 == Solution().findMin([2, 3, 1])
+    assert 1 == Solution().findMin([3, 4, 5, 1, 2])
+    assert 0 == Solution().findMin([4, 5, 6, 7, 0, 1, 2])
+    assert 11 == Solution().findMin([11, 13, 15, 17])
