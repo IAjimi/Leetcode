8855ebe3401e526eb50c351af151bc5681d92234
I A <ia767@nyu.edu>
2022-03-12 09:09:42 -0500

    [162] refactor solution

--- 162_Find_Peak_Element.py
+++ 162_Find_Peak_Element.py
@@ -1,19 +1,32 @@
+from typing import List
+
+
 class Solution:
-	def findPeakElement(self, nums: List[int]) -> int:
-		"""
-		Runtime: 48 ms, faster than 50.60% of Python3 online submissions.
-		Memory Usage: 14.5 MB, less than 11.34% of Python3 online submissions.
+    def findPeakElement(self, nums: List[int]) -> int:
+        """
+        Key element is is that all values in nums are different from their neighbor.
 
-		Meant to be done with binary search for log(n)
-		https://leetcode.com/problems/find-peak-element/discuss/50259/My-clean-and-readable-python-solution
-		"""
-		if len(nums) == 1:
-			return 0
+        Runtime: 58 ms, faster than 64.17% of Python3 online submissions for Find Peak Element.
+                Memory Usage: 14 MB, less than 91.06% of Python3 online submissions for Find Peak Element.
+        """
+        if len(nums) == 1:
+            return 0
 
-		for ix in range(len(nums)):
-			if ix == 0 and nums[ix] > nums[ix + 1]:
-				return 0
-			elif ix < len(nums) - 1 and nums[ix] > nums[ix + 1] and nums[ix] > nums[ix - 1]:
-				return ix
-			elif ix == len(nums) - 1 and nums[ix] > nums[ix - 1]:
-				return len(nums) - 1
+        left = 0
+        right = len(nums) - 1
+
+        while 0 <= left <= right <= len(nums) - 1:
+            mid = (left + right) // 2
+
+            if mid == 0 and nums[mid] > nums[mid + 1]:
+                return mid
+            elif mid == len(nums) - 1 and nums[mid] > nums[mid - 1]:
+                return mid
+            elif nums[mid] > nums[mid - 1] and nums[mid] > nums[mid + 1]:
+                return mid
+            elif nums[mid] < nums[mid + 1]:
+                left = mid + 1
+            else:
+                right = mid - 1
+
+        return -1
