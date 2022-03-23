2f9cc1dadaf08205e5bd4641d9ff2e737dc90e12
I A <ia767@nyu.edu>
2022-03-23 08:01:19 -0400

    [62] refactor solution

--- 62_Unique_Paths.py
+++ 62_Unique_Paths.py
@@ -1,24 +1,14 @@
 class Solution:
     def uniquePaths(self, m: int, n: int) -> int:
         """
-        Note: paths should actually be initialized at 1. The first row will always be
-        only 1s, so can do range(1, n), range(1, m) instead.
-        Note 2: can also just use 1 array instead of a matrix!
-
-        Runtime: 36 ms, faster than 28.89% of Python3 online submissions.
-        Memory Usage: 14.3 MB, less than 65.63% of Python3 online submissions.
+        Runtime: 39 ms, faster than 67.02% of Python3 online submissions for Unique Paths.
+        Memory Usage: 13.9 MB, less than 42.53% of Python3 online submissions for Unique Paths.
         """
-        if m == 1 or n == 1:
-            return 1
-        else:
-            paths = [[0] * n for i in range(m)]
-            paths[0][0] = 0
-            paths[0][1] = 1
-            paths[1][0] = 1
+        dp = [[1 for _ in range(n)] for _ in range(m)]
 
-            for ix in range(n):
-                for jx in range(m):
-                    if (jx, ix) not in [(0, 0), (0, 1), (1, 0)]:
-                        paths[jx][ix] = max(paths[jx - 1][ix] + paths[jx][ix - 1], paths[jx][ix])
+        for row in range(m):
+            for col in range(n):
+                if row != 0 and col != 0:
+                    dp[row][col] = dp[row - 1][col] + dp[row][col - 1]
 
-            return paths[-1][-1]
+        return dp[-1][-1]
